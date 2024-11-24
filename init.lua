bc5f1f804363a8ecd71fde20a5a86d801043e1cc
aaronapple <aaron.roman.apple@gmail.com>
2024-11-24 15:05:49 -0600

    better markdown viewing and fix tab breaks

--- init.lua
+++ init.lua
@@ -156,6 +156,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- set sane tabstops?
+-- vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -228,7 +233,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -616,7 +621,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -913,6 +918,8 @@ require('lazy').setup({
       require('mini.bracketed').setup()
       -- Navigate using square brackets and some modifier
       -- t for treesitter nodes, c for comment blocks, see help for more
+      require('mini.icons').setup()
+      -- icons....
     end,
   },
   { -- Highlight, edit, and navigate code
